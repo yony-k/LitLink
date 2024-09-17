106a743dc547778af0138a81e12a9b28fdfb8e8c
YeonHee Kim <kyh03179@gmail.com>
2024-09-17 20:30:10 +0900

    [SETTING] 스켈레톤 프로젝트 생성

---
+++ src/main/java/com/yonyk/litlink/LitLinkApplication.java
@@ -0,0 +1,13 @@
+package com.yonyk.litlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonyk/litlink/LitLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonyk.litlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
