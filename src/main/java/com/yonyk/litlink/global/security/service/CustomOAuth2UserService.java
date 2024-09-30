98420a9a4d30dbeaede695e445af7bce4e921ec9
YeonHee Kim <kyh03179@gmail.com>
2024-09-30 10:35:21 +0900

    [TEST] 카카오 로그인 진행 체크

--- src/main/java/com/yonyk/litlink/global/security/service/CustomOAuth2UserService.java
+++ src/main/java/com/yonyk/litlink/global/security/service/CustomOAuth2UserService.java
@@ -29,12 +29,12 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     // OAuth2 제공자로부터 사용자 정보 얻어오기
     Map<String, Object> oAuth2UserAttributes = super.loadUser(userRequest).getAttributes();
 
-    System.out.println("체크1");
+    log.info("체크1");
 
     // OAuth2 제공자 타입 가져오기
     String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-    System.out.println("체크2");
+    log.info("체크2");
 
     // 사용자 식별 정보 가져오기
     String userNameAttributeName =
@@ -44,18 +44,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             .getUserInfoEndpoint()
             .getUserNameAttributeName();
 
-    System.out.println("체크3" + userNameAttributeName);
+    log.info("체크3" + userNameAttributeName);
 
     // 사용자 정보 1차 변환
     OAuth2UserDTO oAuth2UserDTO =
         OAuth2UserDTO.of(registrationId, userNameAttributeName, oAuth2UserAttributes);
 
-    System.out.println("체크4" + oAuth2UserDTO);
+    log.info("체크4" + oAuth2UserDTO);
 
     // 등록된 회원인지 확인 후 회원가입 or 등록된 Member 객체 가져오기
     Member member = memberService.getOrRegister(oAuth2UserDTO);
 
-    System.out.println("체크5" + member);
+    log.info("체크5" + member);
 
     // 시큐리티 세션에 등록
     return new PrincipalDetails(member, oAuth2UserAttributes);
