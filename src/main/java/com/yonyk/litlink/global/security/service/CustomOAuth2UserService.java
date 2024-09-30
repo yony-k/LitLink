3f6be02f3b41b16281fca1ad0cf26c306443fb54
YeonHee Kim <kyh03179@gmail.com>
2024-09-30 13:29:08 +0900

    [REFACTOR] 배포 테스트 완료

--- src/main/java/com/yonyk/litlink/global/security/service/CustomOAuth2UserService.java
+++ src/main/java/com/yonyk/litlink/global/security/service/CustomOAuth2UserService.java
@@ -25,17 +25,11 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-    log.info("체크0");
-
     // OAuth2 제공자로부터 사용자 정보 얻어오기
     Map<String, Object> oAuth2UserAttributes = super.loadUser(userRequest).getAttributes();
 
-    log.info("체크1");
-
     // OAuth2 제공자 타입 가져오기
     String registrationId = userRequest.getClientRegistration().getRegistrationId();
-
-    log.info("체크2");
 
     // 사용자 식별 정보 가져오기
     String userNameAttributeName =
@@ -45,18 +39,12 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             .getUserInfoEndpoint()
             .getUserNameAttributeName();
 
-    log.info("체크3" + userNameAttributeName);
-
     // 사용자 정보 1차 변환
     OAuth2UserDTO oAuth2UserDTO =
         OAuth2UserDTO.of(registrationId, userNameAttributeName, oAuth2UserAttributes);
 
-    log.info("체크4" + oAuth2UserDTO);
-
     // 등록된 회원인지 확인 후 회원가입 or 등록된 Member 객체 가져오기
     Member member = memberService.getOrRegister(oAuth2UserDTO);
-
-    log.info("체크5" + member);
 
     // 시큐리티 세션에 등록
     return new PrincipalDetails(member, oAuth2UserAttributes);
